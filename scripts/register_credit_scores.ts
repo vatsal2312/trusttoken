29874f314a56b4b21f51e0dcd4a7fec00385f49e
Hal Hyatt <feewet@gmail.com>
2021-06-28 14:20:48 -0400

    update script

--- scripts/register_credit_scores.ts
+++ scripts/register_credit_scores.ts
@@ -43,11 +43,16 @@ async function setScore (oracle, score, address) {
 
 function getScores () {
   return [
-    { score: 223, address: '0xEF82e7E85061bd800c040D87D159F769a6b85264' }, // Amber Group
-    { score: 159, address: '0xCAFD96A3475aa9afcC66bc5f9FF589C74ce6A4Bc' }, // Invictus
-    { score: 191, address: '0xBc8e650Bac6A7590F19A958e0F57ac97261677f0' }, // Kbit
-    { score: 223, address: '0xdcf45Ec32B553C8274596CD6401dD78A0fAc8CC1' }, // WinterMute
-    { score: 223, address: '0x964d9D1A532B5a5DaeacBAc71d46320DE313AE9C' }, // Alameda
+    { score:  95,  address: '0x186cf5714316F47BC59e30a850615A3f938d7D79' },
+    { score:  191, address: '0x2ae5C897107AcC1d98a4e245D93A20A8b5a83428' },
+    { score:  191, address: '0x6aD71B4DD5BAE567bCF3376fDc48AC5843E19203' },
+    { score:  223, address: '0x964d9D1A532B5a5DaeacBAc71d46320DE313AE9C' },
+    { score:  191, address: '0xBc8e650Bac6A7590F19A958e0F57ac97261677f0' },
+    { score:  159, address: '0xCAFD96A3475aa9afcC66bc5f9FF589C74ce6A4Bc' },
+    { score:  191, address: '0xD5DeE8195AE62bC011A89f1959A7A375cc0DaF38' },
+    { score:  223, address: '0xdcf45Ec32B553C8274596CD6401dD78A0fAc8CC1' },
+    { score:  223, address: '0xEF82e7E85061bd800c040D87D159F769a6b85264' },
+    { score:  127, address: '0xf3537ac805e1ce18AA9F61A4b1DCD04F10a007E9' },
   ]
 }
 
